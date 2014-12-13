bb42b899de2dc5acbd4ebdf94a91757ff761de70
Jim Hinds <jahbini@jahbini.org>
2014-12-13 21:02:57 +0000

    integrate app and server

---
+++ web.js
@@ -0,0 +1,28 @@
+
+var keystone = require('keystone');
+keystone.init({
+  
+  'name': 'StagServExpress',
+  
+  'favicon': 'public/favicon.ico',
+  'less': 'public',
+  'static': ['public'],
+  
+  'views': 'templates/views',
+  'view engine': 'jade',
+  
+  'auto update': true,
+  'mongo': 'mongodb://localhost/StagServExpress',
+  
+  'session': true,
+  'auth': true,
+  'user model': 'User',
+  'cookie secret': 'JimBobway'
+  
+});
+ 
+require('./models');
+ 
+keystone.set('routes', require('./routes'));
+ 
+keystone.start();
