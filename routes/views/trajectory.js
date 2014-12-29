711b62a0c43ff06ec084b680aed1797743d379a4
Jim Hinds <jahbini@jahbini.org>
2014-12-29 05:06:22 +0000

    recieve trajectory as JSON. sync with app in subdir 'public'

--- routes/views/trajectory.js
+++ routes/views/trajectory.js
@@ -5,9 +5,7 @@ exports = module.exports = function(req, res) {
     
     var view = new keystone.View(req, res);
     var t = new Trajectory.model( req.body);
-   /* 
     t.readings = JSON.stringify(req.body.readings);
-    */
     t.save(function(err){
           if (err) {
           console.error(err);
